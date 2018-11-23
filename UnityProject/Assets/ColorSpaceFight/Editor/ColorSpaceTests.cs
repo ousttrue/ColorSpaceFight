7a95ee19467661ca611a0a73f1a11d8b72276190
ousttrue <ousttrue@gmail.com>
2018-11-23 14:26:15 +0900

    Added BlitTest

--- UnityProject/Assets/ColorSpaceFight/Editor/ColorSpaceTests.cs
+++ UnityProject/Assets/ColorSpaceFight/Editor/ColorSpaceTests.cs
@@ -60,5 +60,93 @@ namespace ColorSpaceFight
                 Assert.AreEqual(DXGI_FORMAT.DXGI_FORMAT_R8G8B8A8_TYPELESS, desc.Format);
             }
         }
+
+        static Color32 Read(RenderTexture dst)
+        {
+            var read = new Texture2D(2, 2, TextureFormat.ARGB32, false, true);
+            read.SetPixels32(new Color32[]
+            {
+                Color.black,
+                Color.black,
+                Color.black,
+                Color.black,
+            });
+            read.Apply();
+
+            RenderTexture.active = dst;
+            read.ReadPixels(new Rect(0, 0, 2, 2), 0, 0);
+            read.Apply();
+            RenderTexture.active = null;
+            var pixels = read.GetPixels32();
+            return pixels[0];
+        }
+
+        static Color32 Blit(Texture2D src, RenderTexture dst, Material material, bool? setSRGBWrite = null)
+        {
+            if (setSRGBWrite.HasValue)
+            {
+                GL.sRGBWrite = setSRGBWrite.Value;
+            }
+
+            if (material != null)
+            {
+                Graphics.Blit(src, dst, material);
+            }
+            else
+            {
+                Graphics.Blit(src, dst);
+            }
+
+            return Read(dst);
+        }
+
+        [Test]
+        public void BlitTest()
+        {
+            var shader = Resources.Load<Shader>("Write_05");
+            var material = new Material(shader);
+
+            Assert.False(GL.sRGBWrite);
+
+            {
+                var pixel = Blit(new Texture2D(2, 2),
+                    new RenderTexture(2, 2, 0),
+                    material,
+                    true);
+                Assert.AreEqual(127, pixel.r);
+            }
+
+            {
+                var pixel = Blit(new Texture2D(2, 2),
+                    new RenderTexture(2, 2, 0),
+                    material,
+                    false);
+                Assert.AreEqual(127, pixel.r);
+            }
+
+            {
+                var pixel = Blit(new Texture2D(2, 2, TextureFormat.ARGB32, false, false),
+                    new RenderTexture(2, 2, 0, RenderTextureFormat.ARGB32, RenderTextureReadWrite.sRGB),
+                    material,
+                    true);
+                Assert.AreEqual(127, pixel.r);
+            }
+
+            {
+                var pixel = Blit(new Texture2D(2, 2, TextureFormat.ARGB32, false, false),
+                    new RenderTexture(2, 2, 0, RenderTextureFormat.ARGB32, RenderTextureReadWrite.sRGB),
+                    material,
+                    false);
+                Assert.AreEqual(127, pixel.r);
+            }
+
+            {
+                var pixel = Blit(new Texture2D(2, 2, TextureFormat.ARGB32, false, false),
+                    new RenderTexture(2, 2, 0, RenderTextureFormat.ARGB32, RenderTextureReadWrite.Linear),
+                    material,
+                    false);
+                Assert.AreEqual(127, pixel.r);
+            }
+        }
     }
 }
---
+++ UnityProject/Assets/ColorSpaceFight/Resources/Write_05.shader
@@ -0,0 +1,47 @@
+﻿Shader "Unlit/NewUnlitShader"
+{
+	Properties
+	{
+	}
+		SubShader
+	{
+		Tags { "RenderType" = "Opaque" }
+		LOD 100
+
+		Pass
+		{
+			CGPROGRAM
+			#pragma vertex vert
+			#pragma fragment frag
+			// make fog work
+			#pragma multi_compile_fog
+
+			#include "UnityCG.cginc"
+
+			struct appdata
+			{
+				float4 vertex : POSITION;
+			};
+
+			struct v2f
+			{
+				UNITY_FOG_COORDS(1)
+				float4 vertex : SV_POSITION;
+			};
+
+			v2f vert(appdata v)
+			{
+				v2f o;
+				o.vertex = UnityObjectToClipPos(v.vertex);
+				UNITY_TRANSFER_FOG(o,o.vertex);
+				return o;
+			}
+
+			fixed4 frag(v2f i) : SV_Target
+			{
+				return fixed4(0.5, 0.5, 0.5, 1.0);
+			}
+			ENDCG
+		}
+	}
+}
